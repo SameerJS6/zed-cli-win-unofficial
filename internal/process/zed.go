feec7d1944b2314abbbbcd304fc2db69ac475479
SameerJS6 <singh6.sameer@gmail.com>
2025-05-26 01:02:01 +0530

    feat: Add Zed process management functions to check if Zed is running and launch it with optional project path

---
+++ internal/process/zed.go
@@ -0,0 +1,62 @@
+package process
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+// isZedRunning checks if the Zed is currently running.
+func isZedRunning() (bool, error) {
+	cmd := exec.Command("powershell", "-NoLogo", "-NoProfile", "-Command", "Get-Process Zed -ErrorAction SilentlyContinue")
+	output, err := cmd.Output()
+
+	if err == nil && len(output) > 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+// LaunchZed launches zed with optional project path
+func LaunchZed(zedPath string, projectPath string) error {
+	var cmd *exec.Cmd
+	isRunning, err := isZedRunning()
+
+	if err != nil {
+		return fmt.Errorf("error checking if Zed is running: %w", err)
+	}
+
+	if isRunning {
+		fmt.Println("Zed is already running in another instance!!")
+		fmt.Println("This CLI cannot launch a second instance due to Zed's limitation")
+		return nil
+	}
+
+	if projectPath != "" {
+		if _, err := os.Stat(projectPath); os.IsNotExist(err) {
+			if err := os.MkdirAll(projectPath, 0755); err != nil {
+				return fmt.Errorf("failed to create folder: %w", err)
+			}
+
+			fmt.Println("Path doesn't exists")
+			fmt.Printf("📁  Created new folder: %s", filepath.Clean(projectPath))
+		}
+
+		cmd = exec.Command(zedPath, projectPath)
+	} else {
+		cmd = exec.Command(zedPath)
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("Error opening Zed: %w", err)
+	}
+
+	fmt.Println("✅  Zed opened successfully!!")
+	return nil
+}
