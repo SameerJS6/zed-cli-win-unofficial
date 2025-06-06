6e14e17403c24af4322d833303c6a0dc1e1c0ad8
SameerJS6 <singh6.sameer@gmail.com>
2025-06-06 20:37:08 +0530

    feat: Implement GetZedVersion function to retrieve and validate Zed executable version, enhancing compatibility checks for running instances

--- internal/process/zed.go
+++ internal/process/zed.go
@@ -6,11 +6,28 @@ import (
 	"os/exec"
 	"path/filepath"
 	"zed-cli-win-unofficial/internal/utils"
+
+	"github.com/bi-zone/go-fileversion"
+	"github.com/hashicorp/go-version"
 )
+
+// GetZedVersion retrieves the version of the Zed executable
+func GetZedVersion(zedPath string) (*version.Version, error) {
+	info, err := fileversion.New(zedPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not get version info for %s: %w", zedPath, err)
+	}
+
+	v, err := version.NewVersion(info.FixedInfo().ProductVersion.String())
+	if err != nil {
+		return nil, fmt.Errorf("could not parse version string: %w", err)
+	}
+	return v, nil
+}
 
 // isZedRunning checks if the Zed is currently running.
 func isZedRunning() (bool, error) {
-	cmd := exec.Command("powershell", "-NoLogo", "-NoProfile", "-Command", "Get-Process Zed -ErrorAction SilentlyContinue")
+	cmd := exec.Command("powershell", "-NoLogo", "-NoProfile", "-Command", "Get-Process Zed-v.176 -ErrorAction SilentlyContinue")
 	output, err := cmd.Output()
 
 	if err == nil && len(output) > 0 {
@@ -23,17 +40,39 @@ func isZedRunning() (bool, error) {
 // LaunchZed launches zed with optional project path
 func LaunchZed(zedPath string, projectPath string) error {
 	var cmd *exec.Cmd
-	isRunning, err := isZedRunning()
+	isRunning, _ := isZedRunning()
+	zedVersion, err := GetZedVersion(zedPath)
 
 	if err != nil {
-		return fmt.Errorf("unable to check if Zed is running: %w", err)
+		utils.Warning(fmt.Sprintf("Could not determine Zed version: %v", err))
+	} else {
+		utils.Debugln(fmt.Sprintf("Current Zed version: %s", zedVersion.String()))
 	}
 
 	if isRunning {
-		utils.Error("Zed is already running in another instance!!")
-		utils.Warning("This CLI cannot launch a second instance due to Zed's limitation")
-		return nil
+		// Constraint for checking old versions of Zed.
+		// Versions less than 0.177.0 cannot open a new project from the CLI if Zed is already running.
+
+		constraint, _ := version.NewConstraint("< 0.177.0")
+		utils.Debugln(fmt.Sprintf("Constraint Checking is %t\n", constraint.Check(zedVersion)))
+
+		// We only block if we could successfully get the version and it matches the constraint.
+		if constraint.Check(zedVersion) {
+			utils.PrintUpgradeRequiredBanner()
+			utils.Warning(fmt.Sprintf("Your current Zed version: v%s", zedVersion.String()))
+			utils.Info("This CLI feature requires Zed v0.177.0 or newer when Zed is already running.\n")
+			utils.Info("💡 Solutions:\n")
+			utils.Info("   1. Update Zed to the latest version (recommended)\n")
+			utils.Info("   2. Close the existing Zed window and try again\n")
+			return nil
+		}
 	}
+
+	// if isRunning {
+	// 	utils.Error("Zed is already running in another instance!!")
+	// 	utils.Warning("This CLI cannot launch a second instance due to Zed's limitation")
+	// 	return nil
+	// }
 
 	if projectPath != "" {
 		if _, err := os.Stat(projectPath); os.IsNotExist(err) {
